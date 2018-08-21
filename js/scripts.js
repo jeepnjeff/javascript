c68ebb1678d7ade64ae98bd61e9f6e60ec088aed
jeepnjeff <jeffreypendergrass@hotmai.com>
2018-08-20 20:57:36 -0700

    end of day 3 class push

--- js/scripts.js
+++ js/scripts.js
@@ -41,10 +41,93 @@
 // <= less than or equal too
 
 // const myNumber = 2
-const userInput = prompt('please enter number')
-if (1 == userInput ) {
-    console.log ("Yoo baby");
-// console.log (!true);
-} else{
-    console.log ("else yoo baby");
-}
+// const userInput = prompt('please enter number')
+// if (1 == userInput ) {
+//     console.log ("Yoo baby");
+// // console.log (!true);
+// } else{
+//     console.log ("else yoo baby");
+// }
+
+// && = and 
+// || pipes mean "or"
+// const userInput = prompt('please enter number')
+// if (userInput == 1 || userInput == 5 || userInput == 15 ) {
+//     console.log("Yoo baby");
+// } else {
+//     console.log("else yoo baby");
+// }
+
+// && = "and" 
+// || pipes mean "or"
+// const userInput = prompt('Please enter number')
+// if (userInput > 5 && userInput < 10) {
+//     console.log("Yoo baby");
+// } else {
+//     console.log("else yoo baby");
+// }
+// day 3 else if statements
+// if (userInput >= 5 ) {
+//     console.log ('Number is greater then five');
+// } else if (userInput < 5 ) {
+//     console.log ('number is less then five');
+// } else if (userInput ==='') {
+// console.log ('user did not enter anything');
+// } else {
+//     console.log ('User did not enter a number')
+// }
+
+
+// Day 3 Switch Statement
+// const today = new Date().getDay();
+// console.log(today);
+
+// switch (today) {
+//     case 0:
+//         console.log('Sunday');
+//         break;
+//     case 1:
+//         console.log('Monday');
+//         break;
+//     case 2:
+//         console.log('Tuesday');
+//         break;
+//     case 3:
+//         console.log('Wednesday');
+//         break;
+//     case 4:
+//         console.log('Thursday');
+//         break;
+//     case 5:
+//         console.log('Friday');
+//         break;
+//     case 6:
+//         console.log('Saturday');
+//         break;
+// }
+
+// // Day 3 Math
+// const addition = 1 + 1;
+// const subtraction = 2 - 1;
+// const multiply = 2 * 2;
+// const divide = 10 / 2;
+// const remainder = 11 % 3;
+// console.log(addition);
+// console.log(subtraction);
+// console.log(multiply);
+// console.log(divide);
+// console.log(remainder);
+
+// let myNum = 1;
+// for (let i = 1; i <= 20; i++) {
+
+// console.log(i);
+// }
+
+
+// let myNum = 5;
+// myNum += 3;
+// myNum -= 1;
+// myNum *= 5;
+// // myNum %= 5;
+// console.log (myNum);
