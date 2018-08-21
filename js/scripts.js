be181da2a45f8826aa8981e332e80f58f863dc3b
jeepnjeff <jeffreypendergrass@hotmai.com>
2018-08-20 19:29:11 -0700

    first push day3

---
+++ js/scripts.js
@@ -0,0 +1,50 @@
+// Day 2 Class Work
+
+// var myName = " Jeff";
+// var myAge = "48";
+// console.log ("Hello my name is" + myName +  ". I am " + myAge + " years old.")
+// const myName = 'Jeff';
+// let myAge = '48';
+// // Log out my name and age es5 way
+// console.log("Hello my name is" + myName + ". I am " + myAge + " years old.");
+// es6 way
+// console.log(`Hello, ${myName}! Your are ${myAge} years old!`);
+
+// const today = new Date();
+// console.log(today);
+// let day = today.getDate();
+// console.log(day);
+// let month = today.getMonth() + 1;
+// let year = today.getFullYear();
+// let hours = today.getHours() - 12;
+// let time = today.getTime();
+
+// console.log(day, month, year, hours);
+// console.log(`${month}/${day}/${year} ${time}`);
+
+// // create a random number between 1-10
+// const rand = Math.floor((Math.random() * 10) + 1);
+// console.log(rand);
+
+// // alert(rand);
+// prompt(rand);
+
+// Day 3 Class Work
+// === Strict comparrison
+// == loose compairison
+// ! Not
+// !== strict not compairison
+// != loose not compairison
+// > greater then 
+// < less than 
+// >= greater then or equal too
+// <= less than or equal too
+
+// const myNumber = 2
+const userInput = prompt('please enter number')
+if (1 == userInput ) {
+    console.log ("Yoo baby");
+// console.log (!true);
+} else{
+    console.log ("else yoo baby");
+}
