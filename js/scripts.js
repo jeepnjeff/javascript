6fb50368baa3c15aca9df93a1923d3383a03023d
jeepnjeff <jeffreypendergrass@hotmai.com>
2018-08-22 20:53:13 -0700

    End class push

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-// Day 2 Class Work
+// !!!!!!!!!!!!!!!!!!!!Day 2 Class Work!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
 // var myName = " Jeff";
 // var myAge = "48";
@@ -29,7 +29,8 @@
 // // alert(rand);
 // prompt(rand);
 
-// Day 3 Class Work
+
+// !!!!!!!!!!!Day 3 Class Work!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 // === Strict comparrison
 // == loose compairison
 // ! Not
@@ -77,8 +78,7 @@
 //     console.log ('User did not enter a number')
 // }
 
-
-// Day 3 Switch Statement
+// !!!!!!!!!!!!!!!!Day 3 Switch Statement!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 // const today = new Date().getDay();
 // console.log(today);
 
@@ -131,3 +131,62 @@
 // myNum *= 5;
 // // myNum %= 5;
 // console.log (myNum);
+
+
+//!!!!!!!!!!!!!!Day 4 class work!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+// let myArray = ['Red', 'Blue', 'Green', 'Purple', 'Black', ' Gray', 'White'];
+// let red = myArray[0];
+// console.log (myArray[1]);
+// console.log (myArray.length);
+// let about = ['Jeff', 22, 'the sky is blue', true];
+// console.log (about);
+//Array minipulation
+// let nums = [1, 2, 3, 4, 5, 6, 7, 8, 9,];
+// nums.push (10); //adds to end of array !!!!!!!!!!!!!!
+// nums.unshift(0); //adds to begining of array
+// nums.pop(); //removes end of array !!!!!!!!!!!!!!!!!!
+// nums.shift();// removes begining item of array
+// nums.splice(6);// removes item after 6 1-6 show!!!!!!!!!!!!!!!!!!!!!!!!!!!
+// nums.splice(6,1)// remove item 7 moves to 6 then removes 1 after!!!!!!!!!!!!!!!
+// nums.splice(2, 4); //keeps 1-2 removes 6 items after removes items in middle of array!!!!!!!!!!!!!!!!!!!!!!
+// let slice = nums.slice(3);// removes first 3 items
+// let slice = nums.slice(2?, 5);
+// console.log(nums);
+// console.log(myArray.indexOf('Green'));//tells what number is in green spot
+// let color = myArray.indexOf('Green');
+// console.log(myArray[color]);// alloows you to find green in array
+// myArray.splice(color);
+// let lastIndex = myArray.lastIndexOf('Red');!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+// console.log (color, 1);
+// console.log(myArray);
+// console.log(nums);
+// console.log (lastIndex);
+// console.log(myArray.reverse());
+// console.log(myArray.sort());
+
+// Day 4 LOOPS!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+// let num = 0;
+// while (num<10){
+//     num++;
+// }
+// for (let i=0; i<10; i++) {
+//     console.log(i);
+// }
+
+// do {
+// num++;
+//     console.log(num)
+// } while (num<10);
+
+let myFavoriteMovies = [];
+let firstFavorite = ['Alien',"1979"];
+let secondFavorite = ['Predator', "1987"];
+let ThirdFavorite = ['Star Wars', "1976"];
+myFavoriteMovies.push(firstFavorite,secondFavorite,ThirdFavorite);
+// console.log (myFavoriteMovies[1][1]);
+let movieYears = [];
+// movieYears.push(myFavoriteMovies[0][1], myFavoriteMovies[1][1], myFavoriteMovies[2][1]);
+for (let i=0; i<myFavoriteMovies.length; i++){
+movieYears.push(myFavoriteMovies[i][1]);
+}
+console.log(movieYears);
