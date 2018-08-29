9564133b076d37b675147e6c12c8b676e811e1de
jeepnjeff <jeffreypendergrass@hotmail.com>
2018-08-28 20:41:43 -0700

    updated to day 5 classwork

--- js/scripts.js
+++ js/scripts.js
@@ -178,15 +178,112 @@
 //     console.log(num)
 // } while (num<10);
 
-let myFavoriteMovies = [];
-let firstFavorite = ['Alien',"1979"];
-let secondFavorite = ['Predator', "1987"];
-let ThirdFavorite = ['Star Wars', "1976"];
-myFavoriteMovies.push(firstFavorite,secondFavorite,ThirdFavorite);
-// console.log (myFavoriteMovies[1][1]);
-let movieYears = [];
-// movieYears.push(myFavoriteMovies[0][1], myFavoriteMovies[1][1], myFavoriteMovies[2][1]);
-for (let i=0; i<myFavoriteMovies.length; i++){
-movieYears.push(myFavoriteMovies[i][1]);
-}
-console.log(movieYears);
+// let myFavoriteMovies = [];
+// let firstFavorite = ['Alien',"1979"];
+// let secondFavorite = ['Predator', "1987"];
+// let ThirdFavorite = ['Star Wars', "1976"];
+// myFavoriteMovies.push(firstFavorite,secondFavorite,ThirdFavorite);
+// // console.log (myFavoriteMovies[1][1]);
+// let movieYears = [];
+// // movieYears.push(myFavoriteMovies[0][1], myFavoriteMovies[1][1], myFavoriteMovies[2][1]);
+// for (let i=0; i<myFavoriteMovies.length; i++){
+// movieYears.push(myFavoriteMovies[i][1]);
+// }
+// console.log(movieYears);
+
+
+
+//!!!!!!!!!!!!!!!!!!!!!!!!!!!Day 5 Class Work!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+var name; //Declare
+name = "Maurice"; //define
+
+//FUNCTIONS
+
+//Anonymous FUNCTIONS
+// function(name, age) {
+//   return `My name is ${name} and I am ${age} years old`;
+// }; //Function DEFINITION
+let todos = [];
+//Named Functions
+function getTodos(question) {
+    todos.push(prompt(question));
+}; //fucntion DEFINITION
+
+// getTodos("Whats the first thing you want to do?"); //Function call site;
+
+// nameThatBand("Coldplay");
+
+function nameThatBand(bandName) { //Function DECLARATION
+    let name = "The Best";
+    return alert(`${bandName} is overrated`);
+};
+
+let sayHello = function() { //function EXPRESSION
+    alert("Hello");
+};
+// sayHello();
+
+let incrementor = 0;
+let sayGoodbye = function() { //RECURSIVE function
+        if (incrementor > 5) {
+            alert("Hello");
+            incrementor++;
+            sayGoodbye();
+        } else {
+            return;
+        }
+    }
+    // sayGoodbye();
+
+//"FAT" Arrow FUNCTIONS
+// () => (); //POINTLESS but basic form
+// 
+// () => { //Arrow funciton that takes 0 args and performs data manipulation
+//   incrementor++;
+//   if(incrementor < 5) {
+//     sayGoodbye();
+//   }
+// };
+// 
+// pasta => { //takes 1 argument (can have parentheses or not) and alerts a string
+//   alert(`My favorite pasta is ${pasta}!`);
+// };
+// 
+// pasta => (pasta.toUpperCase());
+// 
+// (car, house) => {
+//   //call an API to save this data...
+//   return `The returned info is ${data}`;
+// };
+
+
+
+
+//TRY IT OUT 
+
+/** 
+Write a Function that...
+Accepts 1 argument (age of the dog in years).
+Returns the age of the dog using the conversion rate of 1 year to 7 "dog" years.
+Alerts the answer.
+Make sure you're checking for edge-cases!
+*/
+
+function calculateDogYears(age) {
+    let parsed = parseInt(age);
+    if (!isNaN(parsed)) {
+        return parsed * 7;
+    } else {
+        let newAnswer = prompt("Come on! I said a NUMBER of years!", "1");
+        calculateDogYears(newAnswer);
+    }
+};
+
+// let dogAge = prompt("How old is your dog?", "10");
+// alert(calculateDogYears(dogAge));`
+
+
+
+parseInt();
+
+"Ryeker".toUpperCase();
