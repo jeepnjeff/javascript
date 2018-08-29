fed4b51aac43e7e996b218d4a8e449fa4b1a9944
jeepnjeff <jeffreypendergrass@hotmail.com>
2018-08-29 14:12:04 -0700

    finish part 2

--- js/scripts.js
+++ js/scripts.js
@@ -3,42 +3,50 @@
 
 // DONE  Take a number and return the square of that number (power of 2).
 // DONE  If a non-number argument is passed into the function, alert NaN and prompt for another response
-// In a second function, capitalize the first letter of a string and add a period (.) to the end of the string if it doesn't already end with a period
+// DONE  In a second function, capitalize the first letter of a string and add a period (.) to the end of the string if it doesn't already end with a period
 // Create a string that will flip the position of the first and second half of a string. For example, the string “abcdef” would be returned as “defabc”. Hint: use substring.
 // BONUS: If you are bored by all that, write a function that checks if a given string is a palindrome.
 
 //********************Part 1 Return Square of number***************
 
-// function sqRoot() {
-//     let number = parseInt(prompt('Please enter a number.'));
-//     if (number == '' && number == null) { //end script if cancel button is pressed
-//         return;
-//     }
-//     switch (!isNaN(number)) { // is real number = true run sqrt
-//         case true:
-//             alert(Math.sqrt(number));
-//             break;
-//         case (false): // not a real number alert try agian
-//             alert('That is not a number! Please try agian!');
-//             sqRoot();
-//             break;
-//     }
-// }
-// sqRoot();
+function sqRoot() {
+    let number = parseInt(prompt('Please enter a number.'));
+    if (number == '' && number == null) { //end script if cancel button is pressed
+        return;
+    }
+    switch (!isNaN(number)) { // is real number = true run sqrt
+        case true:
+            alert(Math.sqrt(number));
+            break;
+        case (false): // not a real number alert try agian
+            alert('That is not a number! Please try agian!');
+            sqRoot();
+            break;
+    }
+}
+sqRoot();
 
 // //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!Part 2!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
 
 function caps() {
     let string = prompt('Please type in "i am awesome" below');
+    let upper = string.charAt(0).toUpperCase() + string.slice(1);
     console.log(string);
-    switch (true) {
-        case string === null:
+    switch (string) {
+        case string !== null:
             alert('Please enter phrase');
             caps();
             break;
-
+        case string != "" && !null:
+            caps()
+            break;
+        case string.indexOf('.') !== -1:
+            alert(`Period found!\n${upper}`);
+            break;
         default:
+            let period = `${upper}.`;
+            alert(period);
             break;
     }
 }
